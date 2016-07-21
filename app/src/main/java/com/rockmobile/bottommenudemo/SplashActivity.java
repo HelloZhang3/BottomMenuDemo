cea0f54cab345dfc6d921f30ed67dc4c68e205f1
zhangyadong <zhangyadong@rockmobile.com.cn>
2016-07-21 17:59:03 +0800

    遗留问题：Android中launcherMode=＂singleTask＂详解<解决home键返回不能出现原界面问题> 解决：程序入口设置SingleTask就会出现这种情况，添加一个欢迎页作为程序入口，OK了

---
+++ app/src/main/java/com/rockmobile/bottommenudemo/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.rockmobile.bottommenudemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        countDownTimer();
+    }
+
+    /**
+     * 倒计时函数
+     */
+    private void countDownTimer() {
+        new CountDownTimer(1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+}
